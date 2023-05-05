512e43bb1725b5311816179088d33c25da3a1e7b
murilobeppler <127536150+murilobeppler@users.noreply.github.com>
2023-05-05 10:33:01 -0300

    Add files via upload

---
+++ exextra.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int y, i, j;
+    char frase[1000];
+    printf("digite uma frase, para que seja transformada no jeito do personagem cebolinha\n");
+    fgets(frase, 1000, stdin);
+
+    y = strlen(frase);
+
+    for (i = 0; i <= y; i++)
+    {
+        if (frase[i + 1] != ' ' && frase[i +1]!='\0')
+        {
+            if (frase[i] == 'r' || frase[i] == 'R')
+            {
+
+                if (frase[i+1] == 'r' || frase[i+1] == 'R' )
+                {
+
+                    for (j = i; frase[j] != '\0'; j++)
+                    {
+
+                        frase[j] = frase[j+1];
+                    }
+                }
+                frase[i] = 'l';
+            }
+        }
+    }
+    fputs(frase, stdout);
+}
